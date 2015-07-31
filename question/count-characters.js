9cd3bf6265020ac15b8944814356f8ede58425cb
sugarshin <shinsugar@gmail.com>
2015-08-01 00:34:15 +0900

    update

--- question/count-characters.js
+++ question/count-characters.js
@@ -2,16 +2,17 @@
 // abcという文字列を渡した時に、{a: 1, b: 1, c: 1}という値を返すようにします。
 // 記号は計上せず、大文字がある場合は小文字として同じアルファベットに計上します。
 module.exports = function (string) {
-  return string.toLowerCase().split('').filter(function(str) {
-    if (!str.match(/[^a-z]/gi)) {
+  var stringArray = string.toLowerCase().split('');
+  return stringArray.filter(function(str) {
+    if (/[a-z]/.test(str)) {
       return str;
     }
-  }).reduce(function(a, b) {
-    if (!a[b]) {
-      a[b] = 1;
+  }).reduce(function(result, current) {
+    if (!result[current]) {
+      result[current] = 1;
     } else {
-      a[b] += 1;
+      result[current] += 1;
     }
-    return a;
+    return result;
   }, {});
 };
