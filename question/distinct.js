ba764f76c723986cee57cb8277425be145581e07
sato <sato@tsumikiinc.com>
2015-08-07 17:47:22 +0900

    WIP

--- question/distinct.js
+++ question/distinct.js
@@ -15,42 +15,93 @@
 //   hoge: 'abe'
 // }
 // を返却する。
+
 module.exports = function (array) {
+  var length = array.length;
 
-  var duplicateKeys = array.map(function(object) {
-    return Object.keys(object);
-  })
-  // flatten
-  .reduce(function(a, b) {
-    return a.concat(b);
-  }, [])
-  // 重複のみをfilter
-  .filter(function(key, i, keys) {
-    return keys.indexOf(key) === i && i !== keys.lastIndexOf(key);
-  });
+  var count = getCountKeys(array);
+  console.log(count)
 
-  var resultObject = array.reduce(function(prevObject, currentObject) {
-    Object.keys(currentObject).forEach(function(key) {
-      if (duplicateKeys.indexOf(key) !== -1) {
-        if (!prevObject[key]) {
-          prevObject[key] = '' + currentObject[key];
-        } else {
-          prevObject[key] += '' + currentObject[key];
-        }
-      }
-    });
-    return prevObject;
-  }, {});
-
-  Object.keys(resultObject).forEach(function(key) {
-    resultObject[key] = resultObject[key].split('')
-      .filter(function(str, i, array) {
-        return array.indexOf(str) === i;
-      })
-      .sort()
-      .join('');
-  });
-
-  return resultObject;
+  // var duplicateKeys = array.reduce(function(result, current) {
+  //   objectForEach(current, function(key, val, i) {
+  //     var _r = [];
+  //     if (i === 0) {
+  //       result.push(key);
+  //     } else {
+  //       if (result.indexOf(key) !== -1) {
+  //         _r.push(key);
+  //         result.push(key);
+  //       }
+  //     }
+  //   });
+  //   result.indexOf()
+  // }, []);
+  //
+  // var keysAll = array.map(function(object) {
+  //   return Object.keys(object);
+  // })
+  // .reduce(function(a, b) {
+  //   return a.concat(b);
+  // }, []);
+  //
+  // var duplicateKeys = array.map(function(object) {
+  //
+  // var duplicateKeys = array.map(function(object) {
+  //   return Object.keys(object);
+  // })
+  // // flatten
+  // .reduce(function(a, b) {
+  //   return a.concat(b);
+  // }, [])
+  // // 重複のみをfilter
+  // .filter(function(key, i, keys) {
+  //   return keys.indexOf(key) === i && i !== keys.lastIndexOf(key);
+  // });
+  //
+  // var resultObject = array.reduce(function(prevObject, currentObject) {
+  //   Object.keys(currentObject).forEach(function(key) {
+  //     if (duplicateKeys.indexOf(key) !== -1) {
+  //       if (!prevObject[key]) {
+  //         prevObject[key] = '' + currentObject[key];
+  //       } else {
+  //         prevObject[key] += '' + currentObject[key];
+  //       }
+  //     }
+  //   });
+  //   return prevObject;
+  // }, {});
+  //
+  // Object.keys(resultObject).forEach(function(key) {
+  //   resultObject[key] = resultObject[key].split('')
+  //     .filter(function(str, i, array) {
+  //       return array.indexOf(str) === i;
+  //     })
+  //     .sort()
+  //     .join('');
+  // });
+  //
+  // return resultObject;
 
 };
+
+function objectForEach(object, callback) {
+  Object.keys(object).forEach(function (key, i) {
+    callback(key, object[key], i, object);
+  });
+}
+
+function getCountKeys(arrayOfObject) {
+  return arrayOfObject.reduce(function(result, current) {
+    objectForEach(current, function(key, val) {
+      result[key] = result[key] ? result[key] + 1 : 1;
+    });
+    return result;
+  }, {});
+}
+
+// ['a', 'a', 'b'].reduce(function(result, current) {
+//     objectForEach(current, function(key, val) {
+//       result[key] = result[key] ? result[key] + 1 : 1;
+//     });
+//     return result;
+// }, {}); // {a: 2, b: 1}
--- question/simple-object.js
+++ question/simple-object.js
@@ -1,13 +1,112 @@
 //次のようなオブジェクト
-//{
+// {
 //  key1: {N: "12"},
 //  key2: {S: "String"}
-//}
+// }
 //を以下のように変換して返す関数を作成する。
 //{
 //  key1: 12,
 //  key2: "String"
 //}
-module.exports = function (object) {
- 
+
+var _result = {};
+
+module.exports = function simpleObject(object) {
+  objectForEach(object, function(key, val) {
+    _result[key] = val;
+    if (isObject(val)) {
+      val = convert(val);
+      if (isObject(val)) {
+        simpleObject(val);
+      } else {
+        _result[key] = val;
+      }
+    } else if (Array.isArray(val)) {
+      val.forEach(function(el, i) {
+        if (isObject(el)) {
+          el = convert(el);
+          if (isObject(el)) {
+            simpleObject(el);
+          }
+        }
+        _result[key][i] = el;
+      });
+    } else {
+      _result[key] = val;
+    }
+  });
+  // console.log(object)
+  return _result;
+  // objectForEach(object, function(key, i) {
+  //   var value = object[key];
+  //   if (Array.isArray(value)) {
+  //     value.forEach(function(el, i) {
+  //       if (isObject(el)) {
+  //         simpleObject(el);
+  //       }
+  //     });
+  //   } else {
+  //     if (isObject(value)) {
+  //       objectForEach(value, function(k, i) {
+  //         if (k === 'N' || k === 'S') {
+  //           object[key] = (isNaN(val[k])) ? val[k] : +val[k];
+  //         }
+  //       });
+  //     }
+  //   }
+  // });
+  // return object;
 };
+
+function convert(object) {
+  var key = Object.keys(object)[0];
+  var val = object[key];
+  if (key === 'N' || key === 'S') {
+    object = (isNaN(+val)) ? val : +val;
+    // console.log(object)
+  }
+  return object;
+}
+
+// function simplify(object) {
+//   objectForEach(object, function(key, i) {
+//     var val = object[key];
+//     objectForEach(val, function(k, i) {
+//       if (k === 'N' || k === 'S') {
+//         object[key] = (isNaN(val[k])) ? val[k] : +val[k];
+//       }
+//     });
+//   });
+//   return object;
+// }
+
+function simplify(object) {
+  objectForEach(object, function(key, val) {
+    if (isObject(val)) {
+      objectForEach(val, function(k, v) {
+        if (k === 'N' || k === 'S') {
+          val = (isNaN(+v)) ? v : +v;
+        }
+      });
+    } else if (Array.isArray(val)) {
+      val.forEach(function(el, i) {
+        if (isObject(el)) {
+          simplify(el);
+        }
+      });
+    }
+  });
+  return object;
+}
+
+function objectForEach(object, callback) {
+  Object.keys(object).forEach(function (key, i) {
+    callback(key, object[key], i, object);
+  });
+}
+
+function isObject(value) {
+  return (value !== null &&
+          typeof value !== 'undefined' &&
+          Object(value) === value);
+}
