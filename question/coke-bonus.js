ac44c6b60335c1d766617205bbb708ce30951076
sato <sato@tsumikiinc.com>
2015-09-02 17:43:53 +0900

    add coke-bonus.js

--- question/coke-bonus.js
+++ question/coke-bonus.js
@@ -3,5 +3,13 @@
 // 例えば4本買った時に最終的に手に入るコーラは5本です
 // 例えば16本買った時に最終的に手に入るコーラは21本です
 // n本買った時に最終的に手に入るコーラの数を計算してください
-module.exports = function (number) {
+
+var RATE = 4;
+
+module.exports = function cokeBonus(number) {
+  if (number > 0) {
+    return number + Math.floor((number - 1) / (RATE - 1));
+  } else {
+    return 0;
+  }
 };
